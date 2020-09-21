35c9838079afc238433eb0e10d6c22dc1223f543
Administrator <bruno.carvalho@ime.eb.br>
2020-09-21 12:59:12 -0300

    Doing automaton

--- Lexical.cs
+++ Lexical.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 namespace SimpleScript
 {
     class Lexical
     {
         public String File { get; set; }
-        private String[] NameTable;
+        private List<String> IdTable;
+        private List<String> StringvalTable;
+        private List<int> NumeralTable;
+        private List<char> CharTable;
         public Lexical(String file)
         {
             File = file;
@@ -122,9 +126,315 @@ namespace SimpleScript
                 Token.Secundary = null;
                 return Token;
             }
+            if (name.Equals(":"))
+            {
+                Token.Primary = PrimaryToken.COLON;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals(";"))
+            {
+                Token.Primary = PrimaryToken.SEMI_COLON;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals(","))
+            {
+                Token.Primary = PrimaryToken.COMMA;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("["))
+            {
+                Token.Primary = PrimaryToken.LEFT_SQUARE;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("]"))
+            {
+                Token.Primary = PrimaryToken.RIGHT_SQUARE;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("{"))
+            {
+                Token.Primary = PrimaryToken.LEFT_BRACES;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("}"))
+            {
+                Token.Primary = PrimaryToken.RIGHT_BRACES;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("("))
+            {
+                Token.Primary = PrimaryToken.LEFT_PARENTHESIS;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals(")"))
+            {
+                Token.Primary = PrimaryToken.RIGHT_PARENTHESIS;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("&&"))
+            {
+                Token.Primary = PrimaryToken.AND;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("||"))
+            {
+                Token.Primary = PrimaryToken.OR;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("<"))
+            {
+                Token.Primary = PrimaryToken.LESS_THAN;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals(">"))
+            {
+                Token.Primary = PrimaryToken.GREATER_THAN;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("<="))
+            {
+                Token.Primary = PrimaryToken.LESS_OR_EQUAL;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals(">="))
+            {
+                Token.Primary = PrimaryToken.GREATER_OR_EQUAL;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("!="))
+            {
+                Token.Primary = PrimaryToken.NOT_EQUAL;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("=="))
+            {
+                Token.Primary = PrimaryToken.EQUAL_EQUAL;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("+"))
+            {
+                Token.Primary = PrimaryToken.PLUS;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("++"))
+            {
+                Token.Primary = PrimaryToken.PLUS_PLUS;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("-"))
+            {
+                Token.Primary = PrimaryToken.MINUS;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("--"))
+            {
+                Token.Primary = PrimaryToken.MINUS_MINUS;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("*"))
+            {
+                Token.Primary = PrimaryToken.TIMES;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("/"))
+            {
+                Token.Primary = PrimaryToken.DIVIDE;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("."))
+            {
+                Token.Primary = PrimaryToken.DOT;
+                Token.Secundary = null;
+                return Token;
+            }
+            if (name.Equals("!"))
+            {
+                Token.Primary = PrimaryToken.NOT;
+                Token.Secundary = null;
+                return Token;
+            }
 
-            //TODO: token secundario de palavras nao reservadas
-            throw new NotImplementedException();
+            return SearchName(name);
+        }
+
+        private Boolean IsNumeral(String name)
+        {
+            if (name.Length < 1) return false;
+
+            foreach (char c in name)
+            {
+                if (IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private Boolean IsDigit(char c)
+        {
+            if ((int)c < (int)'0' || (int)c > (int)'9') return false;
+            else return true;
+        }
+
+        private Boolean IsCharacter(char c)
+        {
+            if ((int)c >= (int)'A' && (int)c <= (int)'Z') return true;
+            if ((int)c >= (int)'a' && (int)c <= (int)'z') return true;
+            return false;
+        }
+
+        private Boolean IsId(String name)
+        {
+            if (name.Length < 1) return false;
+
+            if (!IsCharacter(name[0])) return false;
+
+            foreach (char c in name)
+            {
+                if (!IsCharacter(c) && !IsDigit(c)) return false;
+            }
+
+            return true;
+        }
+
+        private Token SearchName(String name)
+        {
+            var Token = new Token();
+            Token.Secundary = null;
+            
+            if (name.Length == 3 && name[0] == '\'' && name[2] == '\'')
+            {
+                Token.Primary = PrimaryToken.CHARACTER;
+
+                var character = name[1];
+
+                for (int i = 0; i < CharTable.Count; i++)
+                {
+                    if (character == CharTable[i])
+                    {
+                        Token.Secundary = i;
+                        break;
+                    }
+                }
+
+                if (!Token.Secundary.HasValue)
+                {
+                    CharTable.Add(character);
+                    Token.Secundary = CharTable.Count - 1;
+                }
+            }
+            else if (name[0] == '\"' && name[name.Length-1] == '\"')
+            {
+                Token.Primary = PrimaryToken.STRINGVAL;
+
+                var stringval = name.Substring(1, name.Length - 2);
+
+                for (int i = 0; i < StringvalTable.Count; i++)
+                {
+                    if (stringval.Equals(StringvalTable[i]))
+                    {
+                        Token.Secundary = i;
+                        break;
+                    }
+                }
+
+                if (!Token.Secundary.HasValue)
+                {
+                    StringvalTable.Add(stringval);
+                    Token.Secundary = StringvalTable.Count - 1;
+                }
+            }
+            else if (IsNumeral(name))
+            {
+                Token.Primary = PrimaryToken.NUMERAL;
+
+                var numeral = Int32.Parse(name);
+
+                for (int i = 0; i < NumeralTable.Count; i++)
+                {
+                    if (numeral == NumeralTable[i])
+                    {
+                        Token.Secundary = i;
+                        break;
+                    }
+                }
+
+                if (!Token.Secundary.HasValue)
+                {
+                    NumeralTable.Add(numeral);
+                    Token.Secundary = NumeralTable.Count - 1;
+                }
+            }
+            else if (IsId(name))
+            {
+                Token.Primary = PrimaryToken.ID;
+
+                for (int i = 0; i < IdTable.Count; i++)
+                {
+                    if (name.Equals(IdTable[i]))
+                    {
+                        Token.Secundary = i;
+                        break;
+                    }
+                }
+
+                if (!Token.Secundary.HasValue)
+                {
+                    IdTable.Add(name);
+                    Token.Secundary = IdTable.Count - 1;
+                }
+            }
+            else
+            {
+                Token.Primary = PrimaryToken.UNKNOWN;
+            }
+
+            return Token;
+        }
+
+        public char GetCharConst(int SecundaryToken)
+        {
+            return CharTable[SecundaryToken];
+        }
+
+        public String GetStringConst(int SecundaryToken)
+        {
+            return StringvalTable[SecundaryToken];
+        }
+
+        public int GetIntConst(int SecundaryToken)
+        {
+            return NumeralTable[SecundaryToken];
+        }
+
+        public String GetId(int SecundaryToken)
+        {
+            return IdTable[SecundaryToken];
         }
 
         public Boolean Analyser()
